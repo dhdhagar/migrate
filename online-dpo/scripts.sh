cb78f9c5c2267ddf0c81d88e8515a77aa0f6e128
Peter Phan <ipeterphan@gmail.com>
2025-01-06 02:47:03 -0500

    Added script for running trainer with specified target word

---
+++ online-dpo/scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# accelerate launch train_online_dpo.py --target airbase
+accelerate launch train_online_dpo.py --target birthstone
+# accelerate launch train_online_dpo.py --target cement
+# accelerate launch train_online_dpo.py --target computer
+# accelerate launch train_online_dpo.py --target filament
+# accelerate launch train_online_dpo.py --target machetes
+# accelerate launch train_online_dpo.py --target meatloaf
+# accelerate launch train_online_dpo.py --target mob
+# accelerate launch train_online_dpo.py --target polyethylene
+# accelerate launch train_online_dpo.py --target skillet
