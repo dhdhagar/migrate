666d11456aa12ec791e23cc01d85e5b1e3cbfb20
Peter Phan <ipeterphan@gmail.com>
2025-02-15 07:30:35 -0500

    Refactored and improved structure

--- online-dpo/train_online_dpo.py
+++ online-dpo/train_online_dpo.py
@@ -3,7 +3,6 @@ import time
 import argparse
 import json
 from datetime import datetime
-from datasets import Dataset
 from trl import OnlineDPOConfig
 from SemantleOnlineDPOTrainer import SemantleOnlineDPOTrainer
 from SimPairJudge import SimPairJudge
@@ -20,29 +19,56 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--target", "-t", type=str, default="computer")
     parser.add_argument("--model", "-m", type=str, default="meta-llama/Llama-3.2-1B-Instruct")
+    parser.add_argument("--4bit", action="store_true", default=False)
+    parser.add_argument("--target", "-t", type=str, default="computer")
     parser.add_argument("--strategy", "-s", type=str, default="random")
-    parser.add_argument("--batch_size", "-b", type=int, default=10)
+    parser.add_argument("--num_guesses", "-b", type=int, default=10)
     parser.add_argument("--n_reps", "-n", type=int, default=1)
     parser.add_argument("--steps", type=int, default=100)
-    parser.add_argument("--warmstart", type=int, default=0)
+    parser.add_argument("--warmstart", type=float, default=0)
     parser.add_argument("--g", type=int, default=5)
+    parser.add_argument("--related", action="store_true", default=False)
+    parser.add_argument("--date", type=str, default="")
     args = parser.parse_args()
     return args
 
 
-if __name__ == "__main__":
-    args = parse_arguments()
-    params = vars(args)
-    # model_name = "HuggingFaceTB/SmolLM-135M-Instruct"
-    # model_name = "Qwen/Qwen2-0.5B-Instruct"
-    model_name = "meta-llama/Llama-3.2-1B-Instruct"
-    # model_name = "meta-llama/Llama-3.2-3B-Instruct"
-    # model_name = "meta-llama/Llama-3.1-8B-Instruct"
-    model_name = params["model"]
-    target_word = params["target"]
+def create_dataset(params):
+    return [
+        {
+            "prompt": [
+                {
+                    "content": "You are a helpful chatbot with high attention to detail who is not talkative and "
+                    "responds only with the answer and no additional conversation. All your responses should be in JSON "
+                    'format, i.e. {key: value}, where the key is always "response" and the value can be a string, int, '
+                    "list, or dict, depending on the context.",
+                    "role": "system",
+                },
+                {
+                    "content": "Your task is to guess a hidden word from the English dictionary. Stick to proper, "
+                    f'single-word English words. Now, guess exactly n={params["num_guesses"]} new word(s) that could be '
+                    "the hidden word. Be creative! (Note: give only a list of word(s) in the provided JSON format, e.g. "
+                    '{"response": ["word1", "word2",...]})',
+                    "role": "user",
+                },
+            ]
+        }
+        for _ in range(params["steps"])
+    ]
 
+
+def setup_logging(params):
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    logdir = f'logs/{params["strategy"]}/{params["date"]}/{params["target"]}'
+    os.makedirs(logdir, exist_ok=True)
+    logfile = f"{logdir}/{timestamp}.log"
+    with open(logfile, "w") as file:
+        json.dump({"Params": params, "Guesses": [], "Related": [], "Final_Sample": ""}, file, indent=4)
+    return logfile
+
+
+def setup_model(params):
     peft_config = LoraConfig(
         r=8,
         lora_alpha=16,
@@ -51,43 +77,40 @@ if __name__ == "__main__":
         task_type="CAUSAL_LM",
         target_modules=["q_proj", "k_proj", "v_proj", "o_proj"],
     )
-    quant_config = BitsAndBytesConfig(
-        load_in_8bit=True,
-        # bnb_4bit_use_double_quant=True,
-        # bnb_4bit_quant_type="nf4",
-        # bnb_4bit_compute_dtype=torch.bfloat16,
-    )
+    if params["4bit"]:
+        quant_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.bfloat16,
+        )
+    else:
+        quant_config = BitsAndBytesConfig(load_in_8bit=True)
+
     model = AutoModelForCausalLM.from_pretrained(
-        model_name, device_map="auto", quantization_config=quant_config, torch_dtype=torch.bfloat16
+        params["model"], device_map="auto", quantization_config=quant_config, torch_dtype=torch.bfloat16
     )
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(params["model"])
     tokenizer.pad_token = tokenizer.eos_token
     model.generation_config.pad_token_id = tokenizer.pad_token_id
-    judge = SimPairJudge(target_word, "princeton-nlp/sup-simcse-roberta-large")
-    num_guesses = params["batch_size"]
-    dataset = [
-        {
-            "prompt": [
-                {
-                    "content": 'You are a helpful chatbot with high attention to detail who is not talkative and responds only with the answer and no \
-additional conversation. All your responses should be in JSON format, i.e. {key: value}, where the key is always \
-"response" and the value can be a string, int, list, or dict, depending on the context.',
-                    "role": "system",
-                },
-                {
-                    "content": f'Your task is to guess a hidden word from the English dictionary. Stick to proper, single-word English words. Now, guess exactly n={num_guesses} new word(s) that could be the hidden word. Be creative! (Note: give only a list of word(s) in the provided JSON format, e.g. {{"response": ["word1", "word2",...]}})',
-                    "role": "user",
-                },
-            ]
-        }
-        for _ in range(params["steps"])
-    ]
-    train_dataset = Dataset.from_list(dataset)
-    del dataset
+    return model, tokenizer, peft_config
+
+
+def main():
+    args = parse_arguments()
+    params = vars(args)
+
+    dataset = create_dataset(params)
+
+    model, tokenizer, peft_config = setup_model(params)
+
+    judge = SimPairJudge(params["target"], "princeton-nlp/sup-simcse-roberta-large")
+
+    logfile = setup_logging(params)
 
     training_args = OnlineDPOConfig(
-        output_dir=f"{model_name}-OnlineDPO",
-        logging_steps=5,
+        output_dir="OnlineDPO",
+        logging_steps=10,
         fp16=False,
         bf16=True,
         use_cpu=True,
@@ -95,26 +118,26 @@ additional conversation. All your responses should be in JSON format, i.e. {key:
         per_device_train_batch_size=1,
         num_train_epochs=1,
         temperature=0.9,
-        max_new_tokens=int(32 + (5 * num_guesses)),
-        beta=[0.1],
+        max_new_tokens=int(32 + (5 * params["num_guesses"])),
+        beta=0.01,
+        # disable_dropout=False,
     )
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    logdir = f'logs/{target_word}/{params["strategy"]}'
-    os.makedirs(logdir, exist_ok=True)
-    logfile = f'{logdir}/{timestamp}_{params["warmstart"]}.log'
     trainer = SemantleOnlineDPOTrainer(
         model=model,
         judge=judge,
         args=training_args,
         processing_class=tokenizer,
-        train_dataset=train_dataset,
+        train_dataset=dataset,
         peft_config=peft_config,
         tokenizer=tokenizer,
-        target=target_word,
-        num_guesses=num_guesses,
+        target=params["target"],
+        num_guesses=params["num_guesses"],
         logfile=logfile,
         strategy=params["strategy"],
         warmstart=params["warmstart"],
+        g=params["g"],
+        n_reps=params["n_reps"],
+        sample_related=params["related"],
     )
     start_time = time.time()
 
@@ -131,23 +154,32 @@ additional conversation. All your responses should be in JSON format, i.e. {key:
     # Sample from final trained model and log
     sample_prompt = [
         {
-            "content": 'You are a helpful chatbot with high attention to detail who is not talkative and responds only with the answer and no \
-additional conversation. All your responses should be in JSON format, i.e. {key: value}, where the key is always \
-"response" and the value can be a string, int, list, or dict, depending on the context.',
-            "role": "system",
-        },
-        {
-            "content": f'Your task is to guess a hidden word from the English dictionary. Stick to proper, single-word English words. Now, guess exactly n=20 new word(s) that could be the hidden word. Be creative! (Note: give only a list of word(s) in the provided JSON format, e.g. {{"response": ["word1", "word2",...]}})',
-            "role": "user",
-        },
+            "prompt": [
+                {
+                    "content": "You are a helpful chatbot with high attention to detail who is not talkative and "
+                    "responds only with the answer and no additional conversation. All your responses should be in JSON "
+                    'format, i.e. {key: value}, where the key is always "response" and the value can be a string, int, '
+                    "list, or dict, depending on the context.",
+                    "role": "system",
+                },
+                {
+                    "content": "Your task is to guess a hidden word from the English dictionary. Stick to proper, "
+                    "single-word English words. Now, guess exactly n=20 new word(s) that could be "
+                    "the hidden word. Be creative! (Note: give only a list of word(s) in the provided JSON format, e.g. "
+                    '{"response": ["word1", "word2",...]})',
+                    "role": "user",
+                },
+            ]
+        }
     ]
     inputs = tokenizer.apply_chat_template(sample_prompt, tokenize=True, return_tensors="pt").to(DEVICE)
     output = model.generate(inputs, num_return_sequences=1, max_new_tokens=512, temperature=0.9)[0][len(inputs[0]) :]
     with open(logfile, "r") as file:
         data = json.load(file)
-    data.append({"Final Sample": tokenizer.decode(output, skip_special_tokens=True)})
+    data["Final_Sample"] = tokenizer.decode(output, skip_special_tokens=True)
     with open(logfile, "w") as file:
         json.dump(data, file, indent=4)
 
-    del inputs
-    del output
+
+if __name__ == "__main__":
+    main()
