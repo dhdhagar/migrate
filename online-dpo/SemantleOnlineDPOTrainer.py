e816dcc56eddac923e63944ed42d0427807f825d
Peter Phan <ipeterphan@gmail.com>
2025-02-16 22:12:41 -0500

    Abstracted prompt completion for sampling related words

--- online-dpo/SemantleOnlineDPOTrainer.py
+++ online-dpo/SemantleOnlineDPOTrainer.py
@@ -125,24 +125,9 @@ class SemantleOnlineDPOTrainer(OnlineDPOTrainer):
             self.warmstart = None
 
     def sample_related_completions(self, model, chosen_completion, n):
-        inputs = {
-            "prompt": [
-                {
-                    "content": "You are a helpful chatbot with high attention to detail who is not talkative and responds "
-                    "only with the answer and no additional conversation. All your responses should be in JSON format, "
-                    'i.e. {key: value}, where the key is always "response" and the value can be a string, int, list, '
-                    "or dict, depending on the context.",
-                    "role": "system",
-                },
-                {
-                    "content": "Your task is to guess words related to a word from the English dictionary. Stick to proper, "
-                    f"single-word English words. Now, guess exactly n={n} new word(s) that could be related to the word "
-                    f'"{chosen_completion}". Be creative! (Note: give only a list of word(s) in the provided JSON format, e.g. '
-                    '{"response": ["word1", "word2",...]})',
-                    "role": "user",
-                },
-            ]
-        }
+        inputs = None
+        if self.task == "semantle":
+            inputs = {"prompt": prompts_getter.get_semantle_related_prompt(n, chosen_completion)}
         inputs = maybe_apply_chat_template(inputs, self.processing_class)
         inputs = [self.tokenize_row(inputs, self.model.config.is_encoder_decoder, self.processing_class)]
         inputs = self.data_collator(inputs)
