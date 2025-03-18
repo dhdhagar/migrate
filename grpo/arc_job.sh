f8a6b0d2376da2b6c239f0a3058784c0cb612a1c
Peter Phan <ipeterphan@gmail.com>
2025-03-18 08:59:37 -0400

    Added slurm job

---
+++ grpo/arc_job.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#SBATCH -c 8
+#SBATCH --mem=50000
+#SBATCH -p gpu-preempt
+#SBATCH -G 1
+#SBATCH --constraint=[a40|l40s|a100&vram80]
+#SBATCH --time 8:00:00
+#SBATCH -A pi_mccallum_umass_edu
+#SBATCH -o logs/slurm-Eval--%j.out
+#SBATCH -e logs/slurm-Eval--%j.err
+
+export HF_HOME="/work/pi_mccallum_umass_edu/pkphan_umass_edu/pkphan/hf_cache"
+export HF_HUB_CACHE="/work/pi_mccallum_umass_edu/pkphan_umass_edu/pkphan/hf_cache/hub"
+
+module load conda/latest
+module load cuda/12.6
+conda activate /work/pi_mccallum_umass_edu/pkphan_umass_edu/pkphan/.conda/dpo
+
+current_time=$(date "+%Y-%m-%d_%H-%M-%S")
+
+# Full 54 tasks
+targets=('00576224' '0c786b71' '0c9aba6e' '17cae0c1' '195ba7dc' '2072aba6' '27a77e38' '281123b4' '31d5ba1a' '32e9702f' '34b99a2b' '3b4c2228' '3d31c5b3' '48131b3c' '4852f2fa' '4cd1b7b2' '506d28a5' '5783df64' '59341089' '5d2a5c43' '60c09cac' '626c0bcc' '62b74c02' '66e6c45b' '66f2d22f' '68b67ca3' '6a11f6da' '6ad5bdfd' '6ea4a07e' '7953d61e' '833dafe3' '8597cfd7' '8ba14f53' '9110e3c5' 'a8610ef7' 'aa18de87' 'af24b4cc' 'b0722778' 'b1fc8b8e' 'bbb1b8b6' 'be03b35f' 'c074846d' 'c8b7cc0f' 'ca8de6ea' 'd017b73f' 'd19f7514' 'e133d23d' 'e345f17b' 'e633a9e5' 'e6de6e8f' 'e99362f0' 'ed74f2f2' 'ed98d772' 'fc754716')
+
+# Some selected tasks for tuning
+# targets=('3d31c5b3')
+# targets=('5783df64')
+# targets=('6a11f6da')
+# targets=('c074846d')
+# targets=('31d5ba1a')
+# targets=('00576224')
+# targets=("0bb8deee")
+# targets=("0a1d4ef5" "0bb8deee" "0c786b71" "0c9aba6e" "12997ef3" "17cae0c1" "195ba7dc" "1990f7a8")
+
+# TTT pretrained model
+model_name="ekinakyurek/marc-8B-finetuned-llama3"
+# model_name="meta-llama/Llama-3.2-1B-Instruct"
+
+for target in "${targets[@]}"; do
+  accelerate launch train_grpo.py --date "$current_time" --model "$model_name" --target "$target" --n_reps 10 --num_guesses 1 --strategy "Greedy_Single" --task "arc"
+done
