3e7e16c166800b9c037a109d3184b544d4adc76e
Peter Phan <ipeterphan@gmail.com>
2025-03-20 01:48:29 -0400

    Reworked ARC dataset creation

--- grpo/prompts.py
+++ grpo/prompts.py
@@ -63,128 +63,105 @@ def get_mol_prompt(batch_size, protein):
 
 
 class ARC:
-    system_prompt_w_context = f"""You are a helpful chatbot with high attention to detail who is not talkative and \
-    responds only with the answer and no additional conversation. Figure out the underlying transformation in the \
-    following examples and apply it to the test case. 
+    system_prompt_w_context = """You are a helpful chatbot with high attention to detail who is not talkative and \
+responds only with the answer and no additional conversation. Figure out the underlying transformation in the \
+following examples and apply it to the test case. 
 
-    Note: the output grid can be of a different shape than the input grid and the input-output grids are python arrays. \
-    Your answer must follow the same format.
+Note: the output grid can be of a different shape than the input grid and the input-output grids are python arrays. \
+Your answer must follow the same format.
 
-    Here are some examples using this transformation:
-    %s
+Here are some examples using this transformation:
+%s
 
-    Now apply the transformation to the provided test case."""
+Now apply the transformation to the provided test case."""
 
-    system_prompt = f"""You are a helpful chatbot with high attention to detail who is not talkative and responds only \
-    with the answer and no additional conversation. There is a specific grid transformation that we want to use on all \
-    input grids. Guess the transformation we want and apply it to the provided test case. 
+    system_prompt = """You are a helpful chatbot with high attention to detail who is not talkative and responds only \
+with the answer and no additional conversation. There is a specific grid transformation that we want to use on all \
+input grids. Guess the transformation we want and apply it to the provided test case. 
 
-    Note: the output grid can be of a different shape than the input grid and the input-output grids are python arrays. \
-    Your answer must follow the same format.
+Note: the output grid can be of a different shape than the input grid and the input-output grids are python arrays. \
+Your answer must follow the same format.
 
-    Now apply the transformation to the provided test case."""
+Now apply the transformation to the provided test case."""
+    
+def create_arc_prompts(possible_context_examples, user_input, do_permutation=True):
+    dataset = []
+    # Loop over all possible context sizes
+    for i in range(len(possible_context_examples) + 1):
+        if do_permutation:
+            context_combinations = list(itertools.permutations(possible_context_examples, i))
+        else:
+            context_combinations = list(itertools.combinations(possible_context_examples, i))
+
+        # Loop over all possible contexts from leaving out i and of size j
+        for context in context_combinations:
+            # Build context examples
+            context_str = ""
+            for example in context:
+                input_str = str(np.array(example['input']))
+                output_str = str(np.array(example['output']))
+                context_str += f"{input_str} -> {output_str}#\n"
+
+            system_prompt = ARC.system_prompt_w_context % context_str if len(context_str) > 0 else ARC.system_prompt
+
+            prompt = [
+                {"content": system_prompt, "role": "system"},
+                {"content": f"{user_input} -> ", "role": "user"},
+            ]
+            dataset.append(prompt)
+    return dataset
 
 
-def get_arc_prompt(task_id, arc_dataset_file, arc_dataset_solution_file,
-                   readable_prompt=True, all_combinations=False, batches_n=4):
+def get_arc_datasets(task_id, arc_dataset_file, arc_dataset_solution_file, minimum_training_size=50, do_permutation=False):
+    """
+    Create ARC training, validation, and testing prompt datasets.
+
+    Parameters:
+    - task_d (str): ARC task id.
+    - arc_dataset_file (str):  Path to ARC dataset file
+    - arc_dataset_solution_file (str): Path to ARC dataset solution file
+    - minimum_training_size (int): Minimum size of the training dataset
+    - do_permutation (bool): Whether ordering should matter for the context examples
+      
+
+    Returns:
+    - training_dataset: A list of dictionaries with the training prompt and the corresponding training solution
+    - validation_dataset: A dictionary with the validation dataset and the validation solution
+    - test_dataset: A dictionary with the test dataset and the test solution
+    """
     with open(arc_dataset_file, "r", encoding="utf-8") as handle:
         data = json.load(handle)
     with open(arc_dataset_solution_file, "r", encoding="utf-8") as handle:
         data_solutions = json.load(handle)
-    training_examples = data[task_id]["train"][1:]  # Leave first example out for validation
-    validation_example = data[task_id]["train"][0]
+    training_examples = data[task_id]['train'][1:]
+    validation_example = data[task_id]['train'][0]
 
-    batches = []
-    # Loop over context sizes starting from the largest
-    for i in range(len(training_examples) - 1, -1, -1):
-        batch = []
-        for j, leave_out in enumerate(training_examples):
-            leave_out_input = np.array(leave_out["input"])
-            leave_out_str = np.array2string(leave_out_input, separator=" ",
-                                            formatter={"all": lambda x: str(x)}) if readable_prompt else str(
-                leave_out_input.tolist())
-            leave_out_output = np.array(leave_out["output"])
+    print("Available input-output examples", len(data[task_id]['train']))
 
-            context_examples = training_examples[:j] + training_examples[j + 1:]
-            if not all_combinations:
-                # Get contexts with i examples
-                context_combinations = list(itertools.combinations(context_examples, i))
-            else:
-                context_combinations = [comb for _i in range(1, i + 1) for comb in
-                                        list(itertools.combinations(context_examples, _i))]
-
-            for context_example in context_combinations:
-                # Process grids into string for prompt
-                context_str = ""
-                for example in context_example:
-                    # For correct grid string representation
-                    input_str = str(np.array(example["input"])).replace(",", "") if readable_prompt \
-                        else str(example["input"])
-                    output_str = str(np.array(example["output"])).replace(",", "") if readable_prompt \
-                        else str(example["output"])
-                    context_str += f"{input_str} -> {output_str}#\n"
-
-                system_prompt = ARC.system_prompt_w_context % context_str if len(context_str) > 0 else ARC.system_prompt
-
-                prompt = [
-                    {"content": system_prompt, "role": "system"},
-                    {"content": f"{leave_out_str} -> ", "role": "user"},
-                ]
-                batch.append({"prompt": prompt, "leave_out_idx": j + 1, "solution": leave_out_output})  # earlier: j + 1
-        random.shuffle(batch)
-        batches.append(batch)
-
-    # Create a dataset with 4 different prompts with the most examples in their context
     training_dataset = []
-    for batch in batches:
-        training_dataset += batch
-        if batches_n is not None and len(training_dataset) >= batches_n:
-            break
-    training_dataset = training_dataset[:batches_n]
+    for i, leave_out in enumerate(training_examples):
+        leave_out_input = str(np.array(leave_out['input']))
+        possible_context_examples = training_examples[:i] + training_examples[i+1:]
+        dataset = create_arc_prompts(possible_context_examples, leave_out_input)
+        dataset = [{"prompt": x, "solution": np.array(leave_out['output'])} for x in dataset]
+        training_dataset += dataset
+    # Multiply the dataset until it's longer than the minimum length
+    print("Training dataset size", len(training_dataset))
+    dataset = training_dataset * ((minimum_training_size // len(training_dataset)) + 1)
+    random.shuffle(dataset)
 
-    # Create validation example
-    context_str = ""
-    for example in training_examples:
-        input_str = str(np.array(example["input"])).replace(",", "") if readable_prompt \
-            else str(example["input"])
-        output_str = str(np.array(example["output"])).replace(",", "") if readable_prompt \
-            else str(example["output"])
-        context_str += f"{input_str} -> {output_str}#\n"
+    all_training_examples = data[task_id]['train']
+    leave_out_input = str(data[task_id]['test'][0]['input'])
+    test_dataset = create_arc_prompts(all_training_examples, leave_out_input)
+    print("Test dataset size", len(test_dataset))
+    test_dataset = {"dataset": test_dataset, "solution": np.array(data_solutions[task_id][0])}
 
-    valid_input_str = str(np.array(validation_example["input"])).replace(",", "") if readable_prompt \
-        else str(validation_example["input"])
-    validation_prompt = [
-        {
-            "content": ARC.system_prompt_w_context % context_str if len(context_str) > 0 else ARC.system_prompt,
-            "role": "system",
-        },
-        {"content": f"{valid_input_str} -> ", "role": "user"},
-    ]
-    validation_example = {"prompt": validation_prompt, "solution": np.array(validation_example["output"])}
 
-    # Create test example
-    test_example = data[task_id]["test"][0]
-    test_solution = data_solutions[task_id][0]
-    context_str = ""
-    for example in data[task_id]["train"]:
-        input_str = str(np.array(example["input"])).replace(",", "") if readable_prompt \
-            else str(example["input"])
-        output_str = str(np.array(example["output"])).replace(",", "") if readable_prompt \
-            else str(example["output"])
-        context_str += f"{input_str} -> {output_str}#\n"
+    dataset = create_arc_prompts(training_examples, validation_example['input'])
+    print("Validation dataset size", len(dataset))
+    validation_dataset = {"dataset": dataset, "solution": validation_example['output']}
 
-    test_input_str = str(np.array(test_example["input"])).replace(",", "") if readable_prompt \
-        else str(test_example["input"])
-    test_prompt = [
-        {
-            "content": ARC.system_prompt_w_context % context_str if len(context_str) > 0 else ARC.system_prompt,
-            "role": "system",
-        },
-        {"content": f"{test_input_str} -> ", "role": "user"},
-    ]
-    test_example = {"prompt": test_prompt, "solution": np.array(test_solution)}
-
-    return training_dataset, validation_example, test_example
+    return training_dataset, validation_dataset, test_dataset
 
 
 def get_prompt(task, batch_size, target):
@@ -192,5 +169,3 @@ def get_prompt(task, batch_size, target):
         return get_semantle_prompt(batch_size)
     elif task == "chem":
         return get_mol_prompt(batch_size, "KIT")
-    elif task == "arc":
-        return get_arc_prompt(batch_size, target)
