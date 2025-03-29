7dd66ee54271dae84c11dec8546923a588c313ac
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2025-03-29 14:04:06 +0530

    Add abilitiy to pass in comma-separated target list to sbatch scripts

--- grpo/arc_job_only_inference.sh
+++ grpo/arc_job_only_inference.sh
@@ -20,7 +20,14 @@ conda activate /work/pi_mccallum_umass_edu/pkphan_umass_edu/pkphan/.conda/dpo
 current_time=$(date "+%Y-%m-%d_%H-%M-%S")
 
 # Full 54 tasks
-targets=('00576224' '0c786b71' '0c9aba6e' '17cae0c1' '195ba7dc' '2072aba6' '27a77e38' '281123b4' '31d5ba1a' '32e9702f' '34b99a2b' '3b4c2228' '3d31c5b3' '48131b3c' '4852f2fa' '4cd1b7b2' '506d28a5' '5783df64' '59341089' '5d2a5c43' '60c09cac' '626c0bcc' '62b74c02' '66e6c45b' '66f2d22f' '68b67ca3' '6a11f6da' '6ad5bdfd' '6ea4a07e' '7953d61e' '833dafe3' '8597cfd7' '8ba14f53' '9110e3c5' 'a8610ef7' 'aa18de87' 'af24b4cc' 'b0722778' 'b1fc8b8e' 'bbb1b8b6' 'be03b35f' 'c074846d' 'c8b7cc0f' 'ca8de6ea' 'd017b73f' 'd19f7514' 'e133d23d' 'e345f17b' 'e633a9e5' 'e6de6e8f' 'e99362f0' 'ed74f2f2' 'ed98d772' 'fc754716')
+default_targets=('00576224' '0c786b71' '0c9aba6e' '17cae0c1' '195ba7dc' '2072aba6' '27a77e38' '281123b4' '31d5ba1a' '32e9702f' '34b99a2b' '3b4c2228' '3d31c5b3' '48131b3c' '4852f2fa' '4cd1b7b2' '506d28a5' '5783df64' '59341089' '5d2a5c43' '60c09cac' '626c0bcc' '62b74c02' '66e6c45b' '66f2d22f' '68b67ca3' '6a11f6da' '6ad5bdfd' '6ea4a07e' '7953d61e' '833dafe3' '8597cfd7' '8ba14f53' '9110e3c5' 'a8610ef7' 'aa18de87' 'af24b4cc' 'b0722778' 'b1fc8b8e' 'bbb1b8b6' 'be03b35f' 'c074846d' 'c8b7cc0f' 'ca8de6ea' 'd017b73f' 'd19f7514' 'e133d23d' 'e345f17b' 'e633a9e5' 'e6de6e8f' 'e99362f0' 'ed74f2f2' 'ed98d772' 'fc754716')
+
+# Accept targets passed in as a comma-separated string
+if [ -z "$1" ]; then
+  targets=("${default_targets[@]}")
+else
+  IFS=',' read -ra targets <<< "$1"
+fi
 
 # TTT pretrained model
 model_name="ekinakyurek/marc-8B-finetuned-llama3"
